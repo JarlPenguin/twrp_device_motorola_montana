61f33b772afc4c9bcaa31ccc8c35fbf03de5816d
Jarl-Penguin <jarlpenguin@outlook.com>
2022-02-21 09:16:39 -0800

    montana: Fixup dt image generation
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- dt_image.mk
+++ dt_image.mk
@@ -1,18 +1,50 @@
 LOCAL_PATH := $(call my-dir)
 
-## Build and run dtbtool
-DTBTOOL := $(HOST_OUT_EXECUTABLES)/dtbToolCM$(HOST_EXECUTABLE_SUFFIX)
-LZ4_DT_IMAGE := $(PRODUCT_OUT)/dt-lz4.img
+ifeq ($(strip $(TARGET_CUSTOM_DTBTOOL)),)
+DTBTOOL_NAME := dtbToolCM
+else
+DTBTOOL_NAME := $(TARGET_CUSTOM_DTBTOOL)
+endif
+
+DTBTOOL := $(HOST_OUT_EXECUTABLES)/$(DTBTOOL_NAME)$(HOST_EXECUTABLE_SUFFIX)
+
+INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
+LZ4_DTIMAGE = $(PRODUCT_OUT)/dt-lz4.img
 
 ifndef TARGET_PREBUILT_DTB
+ifeq ($(strip $(TARGET_CUSTOM_DTBTOOL)),)
+# dtbToolCM will search subdirectories
+possible_dtb_dirs = $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/
+else
+# Most specific paths must come first in possible_dtb_dirs
+possible_dtb_dirs = $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/ $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/
+endif
+
+define build-dtimage-target
+    $(call pretty,"Target dt image: $@")
+    $(hide) for dir in $(possible_dtb_dirs); do \
+        if [ -d "$$dir" ]; then \
+            dtb_dir="$$dir"; \
+            break; \
+        fi; \
+    done; \
+    $(DTBTOOL) $(BOARD_DTBTOOL_ARGS) -o $@ -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ "$$dtb_dir";
+    $(hide) chmod a+r $@
+endef
+
 $(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(INSTALLED_KERNEL_TARGET)
-	$(call pretty,"Target dt image: $@")
-	$(DTBTOOL) $(BOARD_DTBTOOL_ARGS) -o $@ -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/
-	$(hide) chmod a+r $@
-	lz4 -9 < $@ > $(LZ4_DT_IMAGE) || lz4c -c1 -y $@ $(LZ4_DT_IMAGE)
-	$(hide) $(ACP) $(LZ4_DT_IMAGE) $@
-	@echo "Made DT image: $@"
+	$(build-dtimage-target)
+	lz4 -9 < $@ > $(LZ4_DTIMAGE) || lz4c -c1 -y $@ $(LZ4_DTIMAGE)
+	$(hide) $(ACP) $(LZ4_DTIMAGE) $@
+	$(hide) rm $(LZ4_DTIMAGE)
+	@echo -e ${CL_CYN}"Made DT image: $@"${CL_RST}
 else
 $(INSTALLED_DTIMAGE_TARGET): $(TARGET_PREBUILT_DTB) | $(ACP)
 	$(transform-prebuilt-to-target)
 endif
+
+ALL_DEFAULT_INSTALLED_MODULES += $(INSTALLED_DTIMAGE_TARGET)
+ALL_MODULES.$(LOCAL_MODULE).INSTALLED += $(INSTALLED_DTIMAGE_TARGET)
+
+.PHONY: dtimage
+dtimage: $(INSTALLED_DTIMAGE_TARGET)
